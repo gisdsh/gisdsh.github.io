c833221d8bed51c6ad06719c7e3fd9681e36b483
gisdsh <gisdsh@gmail.com>
2026-06-10 17:55:20 -0300

    260610 175500

--- layers/layers.js
+++ layers/layers.js
@@ -27,11 +27,21 @@ var lyr_Establecimientos_1 = new ol.layer.Vector({
                 style: style_Establecimientos_1,
                 popuplayertitle: 'Establecimientos',
                 interactive: true,
-                title: '<img src="styles/legend/Establecimientos_1.png" /> Establecimientos'
-            });
+    title: 'Establecimientos<br />\
+    <img src="styles/legend/Establecimientos_1_0.png" /> ADECO AGROPECUARIA<br />\
+    <img src="styles/legend/Establecimientos_1_1.png" /> ADELQUIVIGNOLO<br />\
+    <img src="styles/legend/Establecimientos_1_2.png" /> AED<br />\
+    <img src="styles/legend/Establecimientos_1_3.png" /> Agropecuaria Don David SA<br />\
+    <img src="styles/legend/Establecimientos_1_4.png" /> Grupo Linke<br />\
+    <img src="styles/legend/Establecimientos_1_5.png" /> MAXI VARELA MSU<br />\
+    <img src="styles/legend/Establecimientos_1_6.png" /> Walter Hauch<br />' });
+var group_Base = new ol.layer.Group({
+                                layers: [lyr_GoogleHybrid_0,],
+                                fold: 'open',
+                                title: 'Base'});
 
 lyr_GoogleHybrid_0.setVisible(true);lyr_Establecimientos_1.setVisible(true);
-var layersList = [lyr_GoogleHybrid_0,lyr_Establecimientos_1];
+var layersList = [group_Base,lyr_Establecimientos_1];
 lyr_Establecimientos_1.set('fieldAliases', {'fid': 'fid', 'CLIENT_NAM': 'CLIENT_NAM', 'FARM_NAME': 'FARM_NAME', 'ORG_NAME': 'ORG_NAME', 'ORG_ID': 'ORG_ID', });
 lyr_Establecimientos_1.set('fieldImages', {'fid': 'TextEdit', 'CLIENT_NAM': 'TextEdit', 'FARM_NAME': 'TextEdit', 'ORG_NAME': 'TextEdit', 'ORG_ID': 'TextEdit', });
 lyr_Establecimientos_1.set('fieldLabels', {'fid': 'no label', 'CLIENT_NAM': 'inline label - visible with data', 'FARM_NAME': 'inline label - visible with data', 'ORG_NAME': 'inline label - visible with data', 'ORG_ID': 'inline label - visible with data', });
--- styles/Establecimientos_1_style.js
+++ styles/Establecimientos_1_style.js
@@ -1,5 +1,65 @@
 var size = 0;
 var placement = 'point';
+function categories_Establecimientos_1(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+                switch(valueStr) {case 'ADECO AGROPECUARIA':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(255,255,255,0.5)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}),fill: new ol.style.Fill({color: 'rgba(89,207,103,0.5)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'ADELQUIVIGNOLO':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(255,255,255,0.5)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}),fill: new ol.style.Fill({color: 'rgba(93,32,208,0.5)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'AED':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(255,255,255,0.5)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}),fill: new ol.style.Fill({color: 'rgba(200,27,53,0.5)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Agropecuaria Don David SA':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(255,255,255,0.5)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}),fill: new ol.style.Fill({color: 'rgba(240,79,224,0.5)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Grupo Linke':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(255,255,255,0.5)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}),fill: new ol.style.Fill({color: 'rgba(72,121,201,0.5)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'MAXI VARELA MSU':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(255,255,255,0.5)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}),fill: new ol.style.Fill({color: 'rgba(239,201,139,0.5)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'Walter Hauch':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(255,255,255,0.5)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1.52}),fill: new ol.style.Fill({color: 'rgba(145,203,44,0.5)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
 
 var style_Establecimientos_1 = function(feature, resolution){
     var context = {
@@ -8,7 +68,7 @@ var style_Establecimientos_1 = function(feature, resolution){
     };
     
     var labelText = ""; 
-    var value = feature.get("");
+    var value = feature.get("ORG_NAME");
     var labelFont = "9.1px \'Open Sans\', sans-serif";
     var labelFill = "#323232";
     var bufferColor = "#fafafa";
@@ -20,12 +80,10 @@ var style_Establecimientos_1 = function(feature, resolution){
     if (exp_label_Establecimientos_1_eval_expression(context) !== null && resolution > 0 && resolution < 140) {
         labelText = String(exp_label_Establecimientos_1_eval_expression(context));
     }
-    var style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(229,182,54,1.0)'}),
-        text: createTextStyle(feature, resolution, labelText, labelFont,
-                              labelFill, placement, bufferColor,
-                              bufferWidth)
-    })];
+    
+    var style = categories_Establecimientos_1(feature, value, size, resolution, labelText,
+                            labelFont, labelFill, bufferColor,
+                            bufferWidth, placement);
 
     return style;
 };
